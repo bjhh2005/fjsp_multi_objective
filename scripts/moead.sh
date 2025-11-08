3d8d2219f9ea7d6d5ec7b5a2e8e3582eb07e4153
bjhh2005 <2467366054@qq.com>
2025-11-08 22:27:58 +0800

    Create moead.sh

---
+++ scripts/moead.sh
@@ -0,0 +1,7 @@
+python main.py --algorithm moead \
+    --pop_size 300 \
+    --max_gen 1000 \
+    --neighborhood_size 40 \
+    --delta 0.85 \
+    --crossover_rate 0.9 \
+    --mutation_rate 0.15 \
